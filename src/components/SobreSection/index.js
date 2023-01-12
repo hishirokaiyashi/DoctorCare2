8c61f15ab07d93fc0b393a58ed3d3ee52476ce65
hishirokaiyashi <hishirokaiyashi@gmail.com>
2023-01-12 14:36:04 +0700

    add file SobreSection

---
+++ src/components/SobreSection/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./SobreSection.css";
+const SobreSection = () => {
+  return (
+    <section className="SobreSection_container">
+      <div className="SobreSection_container_detail">
+        <div className="SobreSection_container_main">
+          <div className="SobreSection_container_left">
+            <img
+              src="/assets/images/322627573_701469964719280_2965971996845624568_n.png"
+              alt="efeito-mask"
+            />
+          </div>
+          <div className="SobreSection_container_right">
+            <p className="SobreSection_container_right_content">SOBRE NÓS</p>
+            <p className="SobreSection_container_right_title">
+              Entenda quem somos e por que existimos
+            </p>
+            <p className="SobreSection_container_right_subcontent">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim. Amet minim mollit
+              non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+              officia consequat duis enim. Amet minim mollit non deserunt
+              ullamco est sit aliqua dolor do amet sint. Velit officia consequat
+              duis enim. Amet minim mollit non deserunt ullamco est sit aliqua
+              dolor do amet sint. Velit officia consequat duis enim. Amet minim
+              mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SobreSection;
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as SobreSection } from "./SobreSection";
