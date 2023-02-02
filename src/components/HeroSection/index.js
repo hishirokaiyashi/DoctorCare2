4819edffedc1fdedd6aee041f03082c671f93b8d
hishirokaiyashi <hishirokaiyashi@gmail.com>
2023-02-03 00:20:28 +0700

    mock-api

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,66 +1,41 @@
 // libs
 import React from "react";
+//components
+import Button from "../Button";
+import HeroSectionNumber from "../HeroSectionNumber";
 // styles
 import './HeroSection.css'
 
 const HeroSection = () => (
-    <section className="HeroSection_container">
-      <div className="HeroSection_container_detail">
-        <div className="HeroSection_container_main">
-          <div className="HeroSection_left">
-            <div className="HeroSection_left_detail">
-              <p className="HeroSection_left_content">
-                BOAS-VINDAS A DOCTORCARE 👋
-              </p>
-              <h1 className="HeroSection_left_title">
-                Assistência médica simplificada para todos
-              </h1>
-              <p className="HeroSection_left_SubContent">
-                Os médicos da DoctorCare vão além dos sintomas para tratar a
-                causa raiz de sua doença e proporcionar uma cura a longo prazo.
-              </p>
-            </div>
-            <div className="HeroSection_left_button">
-              <span className="HeroSection_left_button_icon">
-                <img src="/assets/images/whatsapp2.svg" alt="phone" />
-              </span>
-              <span className="HeroSection_left_button_content">
-                Agende sua consulta
-              </span>
-            </div>
+  <section className="HeroSection_container">
+    <div className="HeroSection_container_detail">
+      <div className="HeroSection_container_main">
+        <div className="HeroSection_left">
+          <div className="HeroSection_left_detail">
+            <p className="HeroSection_left_content">
+              BOAS-VINDAS A DOCTORCARE 👋
+            </p>
+            <h1 className="HeroSection_left_title">
+              Assistência médica simplificada para todos
+            </h1>
+            <p className="HeroSection_left_SubContent">
+              Os médicos da DoctorCare vão além dos sintomas para tratar a
+              causa raiz de sua doença e proporcionar uma cura a longo prazo.
+            </p>
           </div>
-          <div className="HeroSection_right">
-            <img src="/assets/images/314002953_743397407211109_5520526277855823221_n.png" alt="img_hero_banner" />
+          <div className="HeroSection_left_button">
+            <Button />
           </div>
         </div>
-        <div className="HeroSection_bottom_nums">
-          <div className="HeroSection_bottom_nums_list">
-            <p className="HeroSection_bottom_number">
-              +3.500
-            </p>
-            <p className="HeroSection_bottom_title">
-              Pacientes atendidos
-            </p>
-          </div>
-          <div className="HeroSection_bottom_nums_list">
-            <p className="HeroSection_bottom_number">
-              +15
-            </p>
-            <p className="HeroSection_bottom_title">
-              Especialistas disponíveis
-            </p>
-          </div>
-          <div className="HeroSection_bottom_nums_list">
-            <p className="HeroSection_bottom_number">
-              +10
-            </p>
-            <p className="HeroSection_bottom_title">
-              Anos no mercado
-            </p>
-          </div>
+        <div className="HeroSection_right">
+          <img src="/assets/images/314002953_743397407211109_5520526277855823221_n.png" alt="img_hero_banner" />
         </div>
       </div>
-    </section>
-  );
+      <div className="HeroSection_bottom_nums">
+        <HeroSectionNumber />
+      </div>
+    </div>
+  </section>
+);
 
 export default HeroSection;
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
 export { default as HeroSection } from "./HeroSection";
+export { default as HeroSectionNumber } from "./HeroSectionNumber";
+export { default as Button } from "./Button";
+
